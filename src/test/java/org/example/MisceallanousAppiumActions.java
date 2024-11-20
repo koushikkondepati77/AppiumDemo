6b8f1eb8cb0c41f3a76e727c302555f3badbeae3
koushikkondepati77 <koushikkondepati@gmail.com>
2024-11-19 21:59:00 -0600

    Open App using App Package & App Activity name

--- src/test/java/org/example/MisceallanousAppiumActions.java
+++ src/test/java/org/example/MisceallanousAppiumActions.java
@@ -1,27 +1,39 @@
 package org.example;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
-public class MisceallanousAppiumActivities extends BaseTest{
+public class MisceallanousAppiumActions extends BaseTest{
 
-
+    //App Package and App Activity
+    //adb shell dumpsys window | grep -E 'mCurrentFocus'  -MAC
+    //adb shell dumpsys window | findstr mCurrentFocus - Windows
+    //mCurrentFocus=Window{72be09f u0 io.appium.android.apis/io.appium.android.apis.ApiDemos}
 
     @Test
     public void WifiSettingsName() throws URISyntaxException, MalformedURLException, InterruptedException {
 
+        Activity activity = new Activity("io.appium.android.apis","io.appium.android.apis.preference.PreferenceDependencies");
+        ((JavascriptExecutor) driver).executeScript("mobile: startActivity", ImmutableMap.of(
+                "intent", "io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"
+        ));
+
         //locators -> Xpath, css, accessibilityID, id, className, androidUIAutomator
         //tagName[@attribute='value'] -> common syntax for Xpath
-        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+        //driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        //driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
         driver.findElement(By.id("android:id/checkbox")).click();
         DeviceRotation landscape = new DeviceRotation(0,0,90);
         driver.rotate(landscape);
